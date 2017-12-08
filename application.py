4d05fde639bf9c35b54444b438ee83c7c320fcbb
srachiele <31675017+srachiele@users.noreply.github.com>
2017-12-07 20:30:38 -0600

    Add files via upload

--- application.py
+++ application.py
@@ -49,23 +49,17 @@ def login():
     # if user reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # ensure username was submitted
-        if not request.form.get("email"):
-            return render_template("apology.html")
-            # return apology("must provide username")
-
-        # ensure password was submitted
-        elif not request.form.get("password"):
-            return render_template("apology.html")
-            # return apology("must provide password")
+        # HTML form requires that users input data in both fields before the form is submitted
 
         # # query database for username
-        # rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+        ## select for username or email? Not sure yet
+        rows = db.execute("SELECT * FROM users WHERE email = :email", email=request.form.get("email"))
 
         # # ensure username exists and password is correct
-        # if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
-        #     return render_template("apology.html")
-        #     # return apology("invalid username and/or password")
+        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["password"]):
+            return render_template("apology.html")
+            # return some sort of invalid username/password message, STAY ON LOGIN PAGE
+            # jQuery shake effect: https://www.formget.com/form-shake-effect-on-invalid-entry-using-jquery/
 
         # temp user id for mock login
         id = 123
@@ -114,6 +108,7 @@ def register():
                 return render_template("apology.html")
 
         # post to database
+
         post = db.execute("INSERT INTO users (email, password, teacher) VALUES (:email, :uhash, :teacher)",
             email=request.form["email"],
             uhash=pwd_context.hash(request.form.get("password")),
@@ -148,3 +143,19 @@ def logout():
     return render_template("login.html")
 
 
+@app.route("/question", methods=["GET", "POST"])
+@login_required
+def question():
+
+    #make sure a question is asked
+    if request.method == "GET":
+        return render_template("question.html")
+    if request.method == "POST":
+        if not request.form.get("title"):
+            return apology("must provide a title")
+
+        # post question to database
+        db.execute("INSERT INTO questions (title, description, user_id) VALUES(:title, :description, :user_id)",
+                    title = request.form.get("title"), description = request.form.get("description"), user_id=session["user_id"])
+        #return to some page
+    return redirect(url_for("index"))
